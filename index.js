4da21cedbfffb38ad4046b9b8a3b2b076ae8bca2
Brett Polacek <brettpolacek@gmail.com>
2023-01-31 09:21:11 -0500

    phas 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1;
+const num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+let random  = Math.round(Math.random());
+
+const num3 = 10;
+const num4 = 6;
+let mod =  num3 % num4;
+
+let max = Math.max(1, 2, 3, 5, 6, 7, 11, 12, 20);
